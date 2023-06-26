23352d7c3bff2dbb0a004d3f93ec0d2b9db90794
pabi-code <paballotlhaks@gmail.com>
2023-06-26 15:40:57 -0700

    0x05-pointers_arrays_strings_changing int using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 - function takes a pointer to an int and chages its value to 98
+*@n: value to be updated
+*return: 0
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme file for project 0x05-pointers_arrays_strings
