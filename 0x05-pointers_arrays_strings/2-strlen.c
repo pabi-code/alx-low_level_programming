5c55af40b6039647ac966772b8fafe8d946725eb
pabi-code <paballotlhaks@gmail.com>
2023-06-26 18:15:51 -0700

    a function that prints a string, followed by a new line_0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+
+ * _strlen - returns the length of a string
+
+ * @s: string
+
+ * Return: length
+
+ */
+
+int _strlen(char *s)
+
+{
+
+int stringlen = 0;
+
+while (*s != '\0')
+
+{
+
+stringlen++;
+s++;
+}
+return (stringlen);
+}
